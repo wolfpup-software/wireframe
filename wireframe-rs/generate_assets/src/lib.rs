24577a2893654eeadfbf137cc5f7a257c34c48f3
Taylor Vann <brian.t.vann@gmail.com>
2024-11-29 00:44:47 -0800

    walk paths

--- wireframe-rs/generate_assets/src/lib.rs
+++ wireframe-rs/generate_assets/src/lib.rs
@@ -4,41 +4,63 @@
 // .css
 // .js
 
+use std::path;
 use std::path::PathBuf;
-use tokio::fs::read_dir;
+use tokio::fs::{copy, read_dir};
 
 pub async fn generate_assets(
     source_path: PathBuf,
     dest_path: PathBuf,
     ext: &str,
 ) -> Result<(), String> {
-    // walk through directory
-    // add to stack
-
-    let mut dir_stack = Vec::<PathBuf>::new();
-
-    let mut dir_iter = match read_dir(source_path).await {
-        Ok(ditr) => ditr,
+    let abs_path = match path::absolute(&source_path) {
+        Ok(p) => p,
         Err(e) => return Err(e.to_string()),
     };
 
-    // iterate throuh entries
-    // add to dir_stack if directory
-    // otherwise copy file to location
-    while let Ok(entry_attempt) = dir_iter.next_entry().await {
-        if let Some(entry) = entry_attempt {
-            let path = entry.path();
-            if path.is_dir() {
-                dir_stack.push(entry.path().clone());
+    let mut dir_stack = Vec::<PathBuf>::from([abs_path]);
+
+    // while paths to parse is not 0 length
+    while dir_stack.len() != 0 {
+        let mut curr_stack = dir_stack;
+        dir_stack = Vec::<PathBuf>::new();
+
+        // iterate across queued paths
+        for dir_path in curr_stack {
+            // get absolute path
+            let abs_dir = match path::absolute(&dir_path) {
+                Ok(p) => p,
+                Err(e) => return Err(e.to_string()),
+            };
+
+            if abs_dir.is_file() {
+                return Err("found a file in dir stack".to_string());
             }
 
-            if path.is_file() {
-                // check extension
-                // copy file to location
+            let mut dir_iter = match read_dir(abs_dir).await {
+                Ok(ditr) => ditr,
+                Err(e) => return Err(e.to_string()),
+            };
+
+            while let Ok(entry_attempt) = dir_iter.next_entry().await {
+                if let Some(entry) = entry_attempt {
+                    let entry_path = dir_path.join(entry.path());
+                    if entry_path.is_dir() {
+                        dir_stack.push(entry_path.clone());
+                    }
+
+                    if entry_path.is_file() {
+                        // check extension
+                        // copy file to location
+
+                        // difference between source path and current path
+                        // then add difference to target_path
+                    }
+                }
             }
         }
     }
-    
+
     // keep walking
     Ok(())
 }
