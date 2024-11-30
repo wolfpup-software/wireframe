98b6dc49d72e994270354665eed24611cec52178
Taylor Vann <brian.t.vann@gmail.com>
2024-11-29 18:39:50 -0800

    comment out extension filter

--- wireframe-rs/generate_assets/src/lib.rs
+++ wireframe-rs/generate_assets/src/lib.rs
@@ -11,7 +11,7 @@ use tokio::fs::{copy, create_dir_all, read_dir};
 pub async fn generate_assets(
     source_path: PathBuf,
     dest_path: PathBuf,
-    _ext: &str,
+    _top_level_ext: &str,
 ) -> Result<(), String> {
     let source_abs = match path::absolute(&source_path) {
         Ok(p) => p,
@@ -34,7 +34,7 @@ pub async fn generate_assets(
     let mut source_stack = Vec::<PathBuf>::from([source_abs]);
     let mut dest_stack = Vec::<PathBuf>::from([dest_abs]);
 
-    // while paths to parse is not 0 length
+    // while theyre are source paths left
     while source_stack.len() != 0 {
         let curr_source_stack = source_stack;
         source_stack = Vec::<PathBuf>::new();
@@ -42,18 +42,17 @@ pub async fn generate_assets(
         let curr_dest_stack = dest_stack;
         dest_stack = Vec::<PathBuf>::new();
 
-        // iterate across queued paths
-
         let mut curr_itr = curr_source_stack.iter();
         let mut dest_itr = curr_dest_stack.iter();
 
-        // while iterating across paths
+        // iterate across source paths
         while let (Some(src_path), Some(dst_path)) = (curr_itr.next(), dest_itr.next()) {
             let mut dir_iter = match read_dir(&src_path).await {
                 Ok(ditr) => ditr,
                 Err(e) => return Err(e.to_string()),
             };
 
+            // then iterate across entries in source path
             while let Ok(entry_attempt) = dir_iter.next_entry().await {
                 if let Some(entry) = entry_attempt {
                     let source_entry = src_path.join(entry.path());
@@ -70,8 +69,13 @@ pub async fn generate_assets(
 
                     if source_entry.is_file() {
                         // check extension
-                        // copy file to location
+                        // if let Some(ext) = source_entry.extension() {
+                        //     if ext == top_level_ext {
+                        //         let _ = copy(source_entry, dest_entry).await;
+                        //     }
+                        // }
 
+                        // copy file to location
                         let _ = copy(source_entry, dest_entry).await;
                     }
                 }
@@ -79,6 +83,5 @@ pub async fn generate_assets(
         }
     }
 
-    // keep walking
     Ok(())
 }
