04e131a21b53968ed03e2772318ad6e69eec2fe7
Taylor Vann <brian.t.vann@gmail.com>
2024-11-29 18:24:21 -0800

    copy directory recursively

--- wireframe-rs/generate_assets/src/lib.rs
+++ wireframe-rs/generate_assets/src/lib.rs
@@ -6,55 +6,73 @@
 
 use std::path;
 use std::path::PathBuf;
-use tokio::fs::{copy, read_dir};
+use tokio::fs::{copy, create_dir_all, read_dir};
 
 pub async fn generate_assets(
     source_path: PathBuf,
     dest_path: PathBuf,
-    ext: &str,
+    _ext: &str,
 ) -> Result<(), String> {
-    let abs_path = match path::absolute(&source_path) {
+    let source_abs = match path::absolute(&source_path) {
         Ok(p) => p,
         Err(e) => return Err(e.to_string()),
     };
 
-    let mut dir_stack = Vec::<PathBuf>::from([abs_path]);
+    if source_abs.is_file() {
+        return Err("found a file in dir stack".to_string());
+    }
+
+    let dest_abs = match path::absolute(&dest_path) {
+        Ok(p) => p,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    if dest_abs.is_file() {
+        return Err("found a file in dir stack".to_string());
+    }
+
+    let mut source_stack = Vec::<PathBuf>::from([source_abs]);
+    let mut dest_stack = Vec::<PathBuf>::from([dest_abs]);
 
     // while paths to parse is not 0 length
-    while dir_stack.len() != 0 {
-        let mut curr_stack = dir_stack;
-        dir_stack = Vec::<PathBuf>::new();
+    while source_stack.len() != 0 {
+        let curr_source_stack = source_stack;
+        source_stack = Vec::<PathBuf>::new();
+
+        let curr_dest_stack = dest_stack;
+        dest_stack = Vec::<PathBuf>::new();
 
         // iterate across queued paths
-        for dir_path in curr_stack {
-            // get absolute path
-            let abs_dir = match path::absolute(&dir_path) {
-                Ok(p) => p,
-                Err(e) => return Err(e.to_string()),
-            };
 
-            if abs_dir.is_file() {
-                return Err("found a file in dir stack".to_string());
-            }
+        let mut curr_itr = curr_source_stack.iter();
+        let mut dest_itr = curr_dest_stack.iter();
 
-            let mut dir_iter = match read_dir(abs_dir).await {
+        // while iterating across paths
+        while let (Some(src_path), Some(dst_path)) = (curr_itr.next(), dest_itr.next()) {
+            let mut dir_iter = match read_dir(&src_path).await {
                 Ok(ditr) => ditr,
                 Err(e) => return Err(e.to_string()),
             };
 
             while let Ok(entry_attempt) = dir_iter.next_entry().await {
                 if let Some(entry) = entry_attempt {
-                    let entry_path = dir_path.join(entry.path());
-                    if entry_path.is_dir() {
-                        dir_stack.push(entry_path.clone());
+                    let source_entry = src_path.join(entry.path());
+                    let dest_entry = dst_path.join(entry.path());
+
+                    if source_entry.is_dir() {
+                        if let Err(e) = create_dir_all(&dest_entry).await {
+                            return Err(e.to_string());
+                        }
+
+                        source_stack.push(source_entry.clone());
+                        dest_stack.push(dest_entry.clone());
                     }
 
-                    if entry_path.is_file() {
+                    if source_entry.is_file() {
                         // check extension
                         // copy file to location
 
-                        // difference between source path and current path
-                        // then add difference to target_path
+                        let _ = copy(source_entry, dest_entry).await;
                     }
                 }
             }
